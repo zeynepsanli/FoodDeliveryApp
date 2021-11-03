cadb15d3edfcc5782d78077cfc2f6f5a70ef911b
Zeynep Şanlı <zeynep.sanli@stu.fsm.edu.tr>
2021-11-03 16:10:39 +0300

    Restaurant screen

--- screens/Home.js
+++ screens/Home.js
@@ -473,9 +473,10 @@ const Home = ({ navigation }) => {
             <TouchableOpacity
                 style={{ marginBottom: SIZES.padding * 2 }}
             //onPress; navigate to Restaurant screen
+                onPress={() => navigation.navigate("Restaurant", { item, currentLocation })}
             >
                 {/*Image*/}
-                <View>
+                <View style={{ marginBottom: SIZES.padding }}>
                     <Image
                         source={item.photo}
                         resizeMode="cover"
@@ -504,7 +505,64 @@ const Home = ({ navigation }) => {
                 </View>
 
                 {/* Restaurant Info */}
+                <Text style={{ ...FONTS.body2 }}>{item.name}</Text>
 
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        flexDirection: 'row'
+                    }}
+                >
+                    {/* Rating */}
+                    <Image
+                        source={icons.star}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.primary,
+                            marginRight: 10
+                        }}
+                    />
+                    <Text style={{ ...FONTS.body3 }}> {item.rating} </Text>
+
+                    { /*Categories*/}
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginLeft: 10
+                        }}
+                    >
+                        {
+                            item.categories.map((categoryId) => {
+                                return (
+                                    <View
+                                        style={{ flexDirection: 'row' }}
+                                        key={categoryId}
+                                    >
+                                        <Text style={{ ...FONTS.body3 }}> {getCategoryNameById(categoryId)} </Text>
+                                        <Text style={{ ...FONTS.h3, color: COLORS.darkgray }}> . </Text>
+                                    </View>
+                                )
+                            })
+                        }
+
+                        {/*Price*/}
+
+                        {
+                            [1, 2, 3].map((priceRating) => (
+                                <Text
+                                    key={priceRating}
+                                    style={{
+                                        ...FONTS.body3,
+                                        color: (priceRating <= item.priceRating) ? COLORS.black : COLORS.darkgray
+                                    }}
+                                >$</Text>
+                            ))
+                        }
+
+                    </View>
+
+                </View>
             </TouchableOpacity>
         )
 
--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,12 +1,211 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, Image, Animated } from "react-native";
+import { icons, COLORS, SIZES, FONTS } from '../constans'
 
-const Restaurant = () => {
+const Restaurant = ({ route, navigation }) => {
+
+    const [restaurant, setRestaurant] = React.useState(null);
+    const [currentLocation, setCurrentLocation] = React.useState(null);
+
+    React.useEffect(() => {
+        let { item, currentLocation } = route.params;
+
+        setRestaurant(item)
+        setCurrentLocation(currentLocation)
+    })
+
+    function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image
+                        source={icons.back}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                {/*Restaurant Name Section*/}
+                <View style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+
+                    <View
+                        style={{
+                            height: 50,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingHorizontal: SIZES.padding * 3,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGray3
+                        }}
+                    >
+
+                        <Text style={{ ...FONTS.h3 }}> {restaurant?.name} </Text>
+
+                    </View>
+
+                </View>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.list}
+                        resizeMode='contain'
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    function renderFoodInfo() {
+        return (
+            <Animated.ScrollView
+                horizontal
+                pagingEnabled
+                scrollEnabled
+                scrollEventThrottle={16}
+                snapToAlignment="center"
+                showsHorizontalScrollIndicator={false}
+            //onScroll
+            >
+                {
+                    restaurant?.menu.map((item, index) => (
+                        <View
+                            key={`menu-${index}`}
+                            style={{ alignItems: 'center' }}
+                        >
+                            <View style={{ height: SIZES.height * 0.35 }}>
+                                {/*Food Image*/}
+                                <Image
+                                    source={item.photo}
+                                    resizeMode="cover"
+                                    style={{
+                                        width: SIZES.width,
+                                        height: "100%"
+                                    }}
+                                />
+
+                                {/*Quantitiy*/}
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        bottom: -20,
+                                        width: SIZES.width,
+                                        height: 50,
+                                        justifyContent: 'center',
+                                        flexDirection: 'row'
+                                    }}
+                                >
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopLeftRadius: 25,
+                                            borderBottomLeftRadius: 25
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.body1 }}>-</Text>
+                                    </TouchableOpacity>
+
+                                    <View
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center'
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.h2 }}>5</Text>
+                                    </View>
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopRightRadius: 25,
+                                            borderBottomRightRadius: 25
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.body1 }}>+</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            {/* Name & Description */}
+                            <View
+                                style={{
+                                    width: SIZES.width,
+                                    alignItems: 'center',
+                                    marginTop: 15,
+                                    paddingHorizontal: SIZES.padding * 2
+                                }}
+                            >
+                                <Text style={{ marginVertical: 10, textAlign: 'center', ...FONTS.h2 }}>{item.name} - {item.price.toFixed(2)}</Text>
+                                <Text style={{ ...FONTS.body3 }}>{item.description}</Text>
+                            </View>
+                            {/* Calories */}
+                            <View
+                                style={{
+                                    flexDirection: 'row',
+                                    marginTop: 10
+                                }}
+                            >
+                                <Image
+                                    source={icons.fire}
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        marginRight: 10
+                                    }}
+                                />
+
+                                <Text style={{
+                                    ...FONTS.body3, color: COLORS.darygray
+                                }}>{item.calories.toFixed(2)} cal</Text>
+                            </View>
+                        </View>
+                    ))
+                }
+            </Animated.ScrollView>
+        )
+    }
+
     return (
-        <View>
-            <Text>Restaurant</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+            {renderFoodInfo()}
+        </SafeAreaView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2
+    }
+})
 
 export default Restaurant;
